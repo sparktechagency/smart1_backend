4e2a038186fa88a99f6e548f75123d74746a4498
asifaowadud <asifaowadud@gmail.com>
2025-08-25 16:45:38 +0600

    mend

--- src/app/modules/chat/chat.routes.ts
+++ src/app/modules/chat/chat.routes.ts
@@ -6,9 +6,9 @@ import validateRequest from '../../middleware/validateRequest';
 import { chatValidation } from './chat.validation';
 const router = express.Router();
 
-router.post('/group', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER), validateRequest(chatValidation.createGroupChatSchema), ChatController.createGroupChat);
-router.get('/', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER), ChatController.getChat);
+router.post('/group', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER,USER_ROLES.ADMIN,USER_ROLES.SUPER_ADMIN), validateRequest(chatValidation.createGroupChatSchema), ChatController.createGroupChat);
+router.get('/', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER,USER_ROLES.ADMIN,USER_ROLES.SUPER_ADMIN), ChatController.getChat);
 router.post('/chat-with-admin', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER,USER_ROLES.ADMIN,USER_ROLES.SUPER_ADMIN), ChatController.createChatWithAdmin);
-router.post('/:id', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER), ChatController.createOneToOneChat);
+router.post('/:id', auth(USER_ROLES.USER, USER_ROLES.SERVICE_PROVIDER,USER_ROLES.ADMIN,USER_ROLES.SUPER_ADMIN), ChatController.createOneToOneChat);
 
 export const ChatRoutes = router;
