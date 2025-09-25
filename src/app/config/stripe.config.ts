99a7f0efd1f41d12f7f4c0d0095c593b55e635ce
asifaowadud <asifaowadud@gmail.com>
2025-09-25 11:28:17 +0600

    mend

--- src/app/config/stripe.config.ts
+++ src/app/config/stripe.config.ts
@@ -2,7 +2,8 @@ import Stripe from 'stripe';
 import config from '../../config';
 
 const stripe = new Stripe(config.stripe.stripe_secret_key as string, {
-  apiVersion: '2025-05-28.basil',
+     // apiVersion: '2025-05-28.basil',
+     apiVersion: '2025-08-27.basil',
 });
 
 export default stripe;
