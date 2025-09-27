3d39a01ef23d48fb499e14e4cc45323b8a48105a
asifaowadud <asifaowadud@gmail.com>
2025-09-27 15:36:16 +0600

    mend

--- src/server.ts
+++ src/server.ts
@@ -71,7 +71,6 @@
 /* =============================== | no cluster but modified | =============================== */
 /* =============================== | ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️ | =============================== */
 
-
 import { createServer, Server as HttpServer } from 'http';
 import { Server as SocketServer } from 'socket.io';
 import colors from 'colors';
@@ -114,14 +113,27 @@ export async function startServer() {
           // Set up Socket.io server
           socketServer = new SocketServer({
                cors: {
-                    origin: ['http://10.10.7.79:3000'],
+                    origin: [
+                         'http://dashboard.click-serve.com',
+                         'https://dashboard.click-serve.com',
+                         'https://socket.click-serve.com',
+                         'http://157.241.91.8:3001',
+                         'https://157.241.91.8:3001',
+                         'http://157.241.91.8:6003',
+                         'https://157.241.91.8:6003',
+                         'http://10.10.7.102:3001',
+                         'https://10.10.7.102:3001',
+                         'http://10.10.7.102:6003',
+                         'https://10.10.7.102:6003',
+                    ],
+                    credentials: true,
                },
           });
 
           socketServer.listen(socketPort);
           socketHelper.socket(socketServer);
-           //@ts-ignore
-           global.io = socketServer;
+          //@ts-ignore
+          global.io = socketServer;
           logger.info(colors.yellow(`♻️  Socket is listening on ${ipAddress}:${socketPort}`));
      } catch (error) {
           logger.error(colors.red('Failed to start server'), error);
